fd627957e786c7826f60ab1f9b3c24bd9dff179d
carlosfelipepolaniasanabria <carlosf.polanias@unac.edu.co>
2026-02-17 16:23:42 -0500

    Actualizo daño para imagenes

--- routes/products.router.js
+++ routes/products.router.js
@@ -9,12 +9,11 @@ import { verifyToken } from "../middleware/auth.middleware.js";
 import { isAdmin } from "../middleware/admin.middleware.js";
 import { upload } from "../middleware/upload.middleware.js";
 
-
-
 const router = Router();
 
 router.post(
   "/",
+  verifyToken,
   isAdmin,
   upload.single("imagen"), 
   createProduct
